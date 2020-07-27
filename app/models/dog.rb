1f99d0e5f15d14ac3d805119d7337c64905ac091
Kevin Boston <kbos22@gmail.com>
2020-07-27 12:50:31 +0000

    Automatically backed up by Learn

---
+++ app/models/dog.rb
@@ -0,0 +1,10 @@
+class Dog 
+  attr_accessor :name, :breed, :age 
+  @@all = []
+  def initialize(:name => name, :breed => breed, :age => age)
+    @age = age 
+    @name = name 
+    @breed = breed 
+    @@all << self 
+  end
+end
